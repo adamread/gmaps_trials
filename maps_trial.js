7acd5a50bc81307d15612fa306a3e95cc98e5621
Adam Read <adam@aread.ca>
2013-01-18 22:06:29 +0100

    Added places search.
    Creating the markers not working yet. Must use less copypasta.

--- maps_trial.js
+++ maps_trial.js
@@ -23,4 +23,19 @@ function createMap(location) {
 
     var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
     var marker = new google.maps.Marker({map: map, position: location});
+
+    var restaurantRequest = {
+        location: location,
+        radius: 2000,
+        types: ['restaurant']
+    };
+
+    service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(restaurantRequest, function(results, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+            for (var i = 0; i < results.length; i++) {
+                createMarker(results[i]);
+            }
+        }
+    });
 }

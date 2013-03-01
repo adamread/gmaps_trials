7ada60ff5896e9ed7642bf1e26a6a4c39b08b6ce
Adam Read <adam@aread.ca>
2013-03-01 22:38:35 +0100

    Removed second maps lib load, fixed code for markers.
    The maps js library was being loaded twice, so I took one out of the
    html. There are now also markers on the restaurants, but the info box
    doesn't work yet.

--- maps_trial.js
+++ maps_trial.js
@@ -14,10 +14,9 @@ function codeAddress() {
 
 
 function createMap(location) {
-
     var mapOptions = {
         center: location,
-        zoom: 12,
+        zoom: 15,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
 
@@ -27,14 +26,20 @@ function createMap(location) {
     var restaurantRequest = {
         location: location,
         radius: 2000,
-        types: ['restaurant']
+        types: ['restaurant', 'cafe', 'bakery', 'bar']
     };
 
-    service = new google.maps.places.PlacesService(map);
+    var infoWindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map);
     service.nearbySearch(restaurantRequest, function(results, status) {
         if (status == google.maps.places.PlacesServiceStatus.OK) {
             for (var i = 0; i < results.length; i++) {
-                createMarker(results[i]);
+                var place = results[i];
+                var placeMarker = new google.maps.Marker({map: map, position: place.geometry.location});
+                google.maps.event.addListener(placeMarker, 'click', function() {
+                    infoWindow.setContent(placeMarker.name);
+                    infoWindow.open(map, placeMarker);
+                });
             }
         }
     });
